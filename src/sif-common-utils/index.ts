2f32acc7cd36e15ff11c79febad51a13302598c8
Frode Hansen <frode.hansen@bekk.no>
2021-12-21 13:41:41 +0100

    default export durationWeekdaysUtils

--- src/sif-common-utils/index.ts
+++ src/sif-common-utils/index.ts
@@ -2,4 +2,5 @@ export * from './types';
 export * from './dateUtils';
 export * from './dateRangeUtils';
 export * from './durationUtils';
+export * from './durationWeekdaysUtils';
 export * from './workDurationUtils';
