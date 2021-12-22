aece40f9b15bc8bfc942407b70453b3b30a0e1df
Frode Hansen <frode.hansen@bekk.no>
2021-12-22 12:38:17 +0100

    Default export arrayUtils

--- src/sif-common-utils/index.ts
+++ src/sif-common-utils/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './dateUtils';
+export * from './arrayUtils';
 export * from './dateRangeUtils';
 export * from './durationUtils';
 export * from './dateDurationUtils';
