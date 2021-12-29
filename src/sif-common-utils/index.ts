fdd1ba9c19e60b6865cbfcc78643e9ec65e245bb
Frode Hansen <frode.hansen@bekk.no>
2021-12-29 13:35:11 +0100

    export dateFormatter

--- src/sif-common-utils/index.ts
+++ src/sif-common-utils/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './dateUtils';
 export * from './arrayUtils';
+export * from './dateFormatter';
 export * from './dateRangeUtils';
 export * from './durationUtils';
 export * from './durationWeekdaysUtils';
