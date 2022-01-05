313ef46b463a408a0bc6dc43adc9a2fa35bbf24e
Frode Hansen <frode.hansen@bekk.no>
2022-01-05 14:23:18 +0100

    getDurationForWeekday

--- src/sif-common-utils/__tests__/durationWeekdaysUtils.test.ts
+++ src/sif-common-utils/__tests__/durationWeekdaysUtils.test.ts
@@ -1,3 +1,4 @@
+import { DurationWeekdays, getDurationForWeekday } from '..';
 import { summarizeDurationWeekDays } from '../durationWeekdaysUtils';
 
 describe('workDurationUtils', () => {
@@ -23,6 +24,45 @@ describe('workDurationUtils', () => {
             });
             expect(sum.hours).toBe(5);
             expect(sum.minutes).toBe(30);
+        });
+    });
+    describe('getDurationForWeekday', () => {
+        const durations: DurationWeekdays = {
+            monday: { hours: '1', minutes: '0' },
+            tuesday: { hours: '2', minutes: '0' },
+            wednesday: { hours: '3', minutes: '0' },
+            thursday: { hours: '4', minutes: '0' },
+            friday: { hours: '5', minutes: '0' },
+        };
+        it('returns correctly for monday', () => {
+            const result = getDurationForWeekday(durations, 1);
+            expect(result).toBeDefined();
+            expect(result?.hours).toEqual('1');
+            expect(result?.minutes).toEqual('0');
+        });
+        it('returns correctly for tuesday', () => {
+            const result = getDurationForWeekday(durations, 2);
+            expect(result).toBeDefined();
+            expect(result?.hours).toEqual('2');
+            expect(result?.minutes).toEqual('0');
+        });
+        it('returns correctly for wednesday', () => {
+            const result = getDurationForWeekday(durations, 3);
+            expect(result).toBeDefined();
+            expect(result?.hours).toEqual('3');
+            expect(result?.minutes).toEqual('0');
+        });
+        it('returns correctly for thursday', () => {
+            const result = getDurationForWeekday(durations, 4);
+            expect(result).toBeDefined();
+            expect(result?.hours).toEqual('4');
+            expect(result?.minutes).toEqual('0');
+        });
+        it('returns correctly for friday', () => {
+            const result = getDurationForWeekday(durations, 5);
+            expect(result).toBeDefined();
+            expect(result?.hours).toEqual('5');
+            expect(result?.minutes).toEqual('0');
         });
     });
 });
--- src/sif-common-utils/durationWeekdaysUtils.ts
+++ src/sif-common-utils/durationWeekdaysUtils.ts
@@ -1,4 +1,4 @@
-import { NumberDuration, DurationWeekdays, summarizeDurations } from '.';
+import { NumberDuration, DurationWeekdays, summarizeDurations, Duration } from '.';
 
 export const summarizeDurationWeekDays = (weekdays: DurationWeekdays): NumberDuration => {
     return summarizeDurations([
@@ -8,4 +8,23 @@ export const summarizeDurationWeekDays = (weekdays: DurationWeekdays): NumberDur
         weekdays.thursday,
         weekdays.friday,
     ]);
+};
+
+export const getDurationForWeekday = (
+    durationWeekdays: DurationWeekdays,
+    weekday: 1 | 2 | 3 | 4 | 5
+): Duration | undefined => {
+    switch (weekday) {
+        case 1:
+            return durationWeekdays.monday;
+        case 2:
+            return durationWeekdays.tuesday;
+        case 3:
+            return durationWeekdays.wednesday;
+        case 4:
+            return durationWeekdays.thursday;
+        case 5:
+            return durationWeekdays.friday;
+    }
+    return undefined;
 };
